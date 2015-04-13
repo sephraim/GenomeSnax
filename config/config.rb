60a048b360b18505e40908e106bd79fe1a7a3069
Sean Ephraim <sean.ephraim@gmail.com>
2015-04-13 16:18:41 -0500

    Add config file

---
+++ config/config.rb
@@ -0,0 +1,15 @@
+# Database credentials
+CONFIG[:host] = 'localhost'
+CONFIG[:database] = ''
+CONFIG[:username] = ''
+CONFIG[:password] = ''
+
+# Genome build
+CONFIG[:build] = 'hg19'
+
+# File extensions
+CONFIG[:ext_out] = 'out'
+CONFIG[:ext_missing] = 'missing'
+
+# Format
+CONFIG[:format] = 'tab'
